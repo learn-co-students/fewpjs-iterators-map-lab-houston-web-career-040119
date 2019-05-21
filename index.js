88f8d826ca1c9514ef527731334ba1e48906479d
jpavioli <justin.avioli@gmail.com>
2019-05-21 15:43:26 -0500

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,15 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+   return tutorials.map((string) => {
+     let string_arr = string.split(' ')
+     for (var i = 0; i < string_arr.length; i++) {
+       string_arr[i] = capitalize(string_arr[i])
+     }
+     return string_arr.join(" ")
+   })
+}
+
+function capitalize(word){
+  return word.charAt(0).toUpperCase()+word.slice(1)
 }
